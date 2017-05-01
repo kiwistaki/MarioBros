947230e67a0b39f72b640a11028ce107da8a1124
Akkun <paquet.alex@gmail.com>
2017-04-30 21:58:54 -0400

    added box2d gameObjects to game for interaction

--- core/src/com/akkunsoft/mario/Screens/PlayScreen.java
+++ core/src/com/akkunsoft/mario/Screens/PlayScreen.java
@@ -6,9 +6,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -21,9 +31,13 @@ public class PlayScreen implements Screen {
     private OrthographicCamera gameCamera;
     private Viewport gamePort;
     private Hud hud;
+    //Tiled map variables
     private TmxMapLoader mapLoader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+    //Box2d variables
+    private World world;
+    private Box2DDebugRenderer b2dr;
 
     public PlayScreen(MarioBros mb){
         this.game = mb;
@@ -34,13 +48,74 @@ public class PlayScreen implements Screen {
         // create HUD to show score, level and other informations
         this.hud = new Hud(this.game.batch);
         // create a tmx map loader
-        mapLoader = new TmxMapLoader();
+        this.mapLoader = new TmxMapLoader();
         // load level 1-1
-        map = mapLoader.load("level1.tmx");
+        this.map = mapLoader.load("level1.tmx");
         // render level 1-1
-        renderer = new OrthogonalTiledMapRenderer(map);
+        this.renderer = new OrthogonalTiledMapRenderer(map);
         // center the level
         this.gameCamera.position.set(this.gamePort.getWorldWidth()/2, this.gamePort.getWorldHeight()/2,0);
+
+        this.world = new World(new Vector2(0,0), true); //true to sleep objects at rest
+        this.b2dr = new Box2DDebugRenderer();
+
+        BodyDef bodyDef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fixture = new FixtureDef();
+        Body body;
+
+        //Create ground bodies and fixtures
+        for(MapObject ob : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) ob).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixture.shape = shape;
+            body.createFixture(fixture);
+        }
+        //Create pipe bodies and fixtures
+        for(MapObject ob : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) ob).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixture.shape = shape;
+            body.createFixture(fixture);
+        }
+        //Create brick bodies and fixtures
+        for(MapObject ob : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) ob).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixture.shape = shape;
+            body.createFixture(fixture);
+        }
+        //Create coin bodies and fixtures
+        for(MapObject ob : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) ob).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixture.shape = shape;
+            body.createFixture(fixture);
+        }
     }
 
     @Override
@@ -54,7 +129,9 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         //Render the level
-        renderer.render();
+        this.renderer.render();
+        //renderer the Box2DDebugLines
+        this.b2dr.render(this.world, this.gameCamera.combined);
         //Set the batch to now draw what the HUD camera sees
         this.game.batch.setProjectionMatrix(this.hud.stage.getCamera().combined);
         this.hud.stage.draw();
